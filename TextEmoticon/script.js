3d239a26c58e1bf7cdaf965b0da239d73fa2aae1
Datarumble.Glenscales <gscales@datarumble.com>
2015-07-24 10:49:03 +1000

    u

--- TextEmoticon/script.js
+++ TextEmoticon/script.js
@@ -43,7 +43,7 @@ function AddEmoticonToBody(Emoticon) {
                      // Specify HTML in the coercionType parameter
                      // of setSelectedDataAsync.
                      item.body.setSelectedDataAsync(
-                         "<span style='mso-fareast-font-family:\"Times New Roman\"'>[̲̅$̲̅(̲̅<span class=SpellE>ιοο</span>̲̅)̲̅$̲̅]</span>",
+                         "[̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅]",
                          { coercionType: Office.CoercionType.Html, 
                              asyncContext: { var3: 1, var4: 2 } },
                          function (asyncResult) {
