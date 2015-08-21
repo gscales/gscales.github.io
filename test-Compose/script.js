327c85852dcb06c1c1bd91adf8d5cbc0f2ed1def
Glen Scales <gscales@datarumble.com>
2015-08-21 13:32:16 +1000

    d

--- test-Compose/script.js
+++ test-Compose/script.js
@@ -1,16 +1,14 @@
 ﻿// This function is called when Office.js is ready to start your Add-in
 var _mailbox;
 var _Item;
+var _AppGuid = "99429ef8-be83-4ce2-ba79-f4471f89f674";
 
 Office.initialize = function () {
     $(document).ready(function () {
         var item = Office.context.mailbox.item;
-        item.loadCustomPropertiesAsync(customPropsCallback);
-        function customPropsCallback(asyncResult) {
-            var customProps = asyncResult.value;
-            customProps.set("nssplugIn", "true");
-            customProps.saveAsync(saveCallback);
-        }
+        var request = FindItemRequest();
+        var envelope = getSoapEnvelope(request);
+        _mailbox.makeEwsRequestAsync(envelope, callbackFindItems);
         //var request = getItemRequest(_Item.itemId);
         //var envelope = getSoapEnvelope(request);
         
@@ -18,6 +16,54 @@ Office.initialize = function () {
     });
 };
 function saveCallback(asyncResult) {
+}
+function callbackFindItems(asyncResult) {
+    var result = asyncResult.value;
+    var context = asyncResult.context;
+    $('#ChkTest').text(result);
+}
+function getSoapEnvelope(request) {
+    // Wrap an Exchange Web Services request in a SOAP envelope.
+    var result =
+
+    '<?xml version="1.0" encoding="utf-8"?>' +
+    '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"' +
+    '               xmlns:xsd="http://www.w3.org/2001/XMLSchema"' +
+    '               xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"' +
+    '               xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types">' +
+    '  <soap:Header>' +
+    '    <RequestServerVersion Version="Exchange2013" xmlns="http://schemas.microsoft.com/exchange/services/2006/types" soap:mustUnderstand="0" />' +
+    '  </soap:Header>' +
+    '  <soap:Body>' +
+
+    request +
+
+    '  </soap:Body>' +
+    '</soap:Envelope>';
+
+    return result;
+}
+function FindItemRequest() {
+    // Return a GetItem operation request for the subject of the specified item. 
+    var result =
+ '   <m:FindItem Traversal="Shallow">' +
+ '     <m:ItemShape>' +
+ '     <t:BaseShape>IdOnly</t:BaseShape>' +
+ '        <t:AdditionalProperties>' +
+ '           <t:ExtendedFieldURI DistinguishedPropertySetId="PublicStrings" PropertyName=cecp-"' + _AppGuid + ' PropertyType="String" />' +
+ '        </t:AdditionalProperties>' +
+ '       </m:ItemShape>' +
+ '       <m:IndexedPageItemView MaxEntriesReturned="100" Offset="0" BasePoint="Beginning" />' +
+ '       <m:Restriction>' +
+ '         <t:Exists>' +
+ '           <t:ExtendedFieldURI DistinguishedPropertySetId="PublicStrings" PropertyName=cecp-"' + _AppGuid + ' PropertyType="String" />' +
+ '         </t:Exists>' +
+ '       </m:Restriction>' +
+ '       <m:ParentFolderIds>' +
+ '         <t:DistinguishedFolderId Id="drafts" />' +
+ '       </m:ParentFolderIds>' +
+ '     </m:FindItem>';
+    return result;
 }
 
 function addEmoticon(Emoticon) {
