1f3cb5d12dfebafab08c67db5be9054221718c64
Glen Scales <gscales@datarumble.com>
2015-10-21 11:21:30 +1100

    test

--- test/script.js
+++ test/script.js
@@ -24,83 +24,68 @@ if (navigator.getUserMedia){
 } else alert('getUserMedia not supported in this browser.');
 
 
-
-// when key is down
-window.onkeydown = function(e){
-    
-    // if R is pressed, we start recording
-    if ( e.keyCode == 82 ){
-        recording = true;
-        // reset the buffers for the new recording
-        leftchannel.length = rightchannel.length = 0;
-        recordingLength = 0;
-        outputElement.innerHTML = 'Recording now...';
-    // if S is pressed, we stop the recording and package the WAV file
-    } else if ( e.keyCode == 83 ){
-        
-        // we stop recording
-        recording = false;
-        
-        outputElement.innerHTML = 'Building wav file...';
-
-        // we flat the left and right channels down
-        var leftBuffer = mergeBuffers ( leftchannel, recordingLength );
-        var rightBuffer = mergeBuffers ( rightchannel, recordingLength );
-        // we interleave both channels together
-        var interleaved = interleave ( leftBuffer, rightBuffer );
-        
-        // we create our wav file
-        var buffer = new ArrayBuffer(44 + interleaved.length * 2);
-        var view = new DataView(buffer);
-        
-        // RIFF chunk descriptor
-        writeUTFBytes(view, 0, 'RIFF');
-        view.setUint32(4, 44 + interleaved.length * 2, true);
-        writeUTFBytes(view, 8, 'WAVE');
-        // FMT sub-chunk
-        writeUTFBytes(view, 12, 'fmt ');
-        view.setUint32(16, 16, true);
-        view.setUint16(20, 1, true);
-        // stereo (2 channels)
-        view.setUint16(22, 2, true);
-        view.setUint32(24, sampleRate, true);
-        view.setUint32(28, sampleRate * 4, true);
-        view.setUint16(32, 4, true);
-        view.setUint16(34, 16, true);
-        // data sub-chunk
-        writeUTFBytes(view, 36, 'data');
-        view.setUint32(40, interleaved.length * 2, true);
-        
-        // write the PCM samples
-        var lng = interleaved.length;
-        var index = 44;
-        var volume = 1;
-        for (var i = 0; i < lng; i++){
-            view.setInt16(index, interleaved[i] * (0x7FFF * volume), true);
-            index += 2;
-        }
-        
-        // our final binary blob
-        var blob = new Blob ( [ view ], { type : 'audio/wav' } );
-        
-        // let's save it locally
-        outputElement.innerHTML = 'Handing off the file now...';
-        var url = (window.URL || window.webkitURL).createObjectURL(blob);
-        var link = window.document.createElement('a');
-        link.href = url;
-        link.download = 'output.wav';
-        var click = document.createEvent("Event");
-        click.initEvent("click", true, true);
-        link.dispatchEvent(click);
-    }
-}
-
 function b1Click() {
     recording = true;
     // reset the buffers for the new recording
     leftchannel.length = rightchannel.length = 0;
     recordingLength = 0;
     outputElement.innerHTML = 'Recording now...';
+}
+function b2Click() {
+    // we stop recording
+    recording = false;
+
+    outputElement.innerHTML = 'Building wav file...';
+
+    // we flat the left and right channels down
+    var leftBuffer = mergeBuffers(leftchannel, recordingLength);
+    var rightBuffer = mergeBuffers(rightchannel, recordingLength);
+    // we interleave both channels together
+    var interleaved = interleave(leftBuffer, rightBuffer);
+
+    // we create our wav file
+    var buffer = new ArrayBuffer(44 + interleaved.length * 2);
+    var view = new DataView(buffer);
+
+    // RIFF chunk descriptor
+    writeUTFBytes(view, 0, 'RIFF');
+    view.setUint32(4, 44 + interleaved.length * 2, true);
+    writeUTFBytes(view, 8, 'WAVE');
+    // FMT sub-chunk
+    writeUTFBytes(view, 12, 'fmt ');
+    view.setUint32(16, 16, true);
+    view.setUint16(20, 1, true);
+    // stereo (2 channels)
+    view.setUint16(22, 2, true);
+    view.setUint32(24, sampleRate, true);
+    view.setUint32(28, sampleRate * 4, true);
+    view.setUint16(32, 4, true);
+    view.setUint16(34, 16, true);
+    // data sub-chunk
+    writeUTFBytes(view, 36, 'data');
+    view.setUint32(40, interleaved.length * 2, true);
+
+    // write the PCM samples
+    var lng = interleaved.length;
+    var index = 44;
+    var volume = 1;
+    for (var i = 0; i < lng; i++) {
+        view.setInt16(index, interleaved[i] * (0x7FFF * volume), true);
+        index += 2;
+    }
+
+    // our final binary blob
+    var blob = new Blob([view], { type: 'audio/wav' });
+
+    // let's save it locally
+    outputElement.innerHTML = 'Handing off the file now...';
+    var url = (window.URL || window.webkitURL).createObjectURL(blob);
+    var link = window.document.createElement('a');
+    link.href = url;
+    link.download = 'output.wav';
+    var click = document.createEvent("Event");
+    click.initEvent("click", true, true);
+    link.dispatchEvent(click);
 }
 
 function interleave(leftChannel, rightChannel){
