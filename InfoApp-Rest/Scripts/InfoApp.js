61b3587dfe6d1b2dc166c74557bfc03696c8ad47
Glen Scales <gscales@datarumble.com>
2018-05-23 17:31:54 +1000

    changes

--- InfoApp-Rest/Scripts/InfoApp.js
+++ InfoApp-Rest/Scripts/InfoApp.js
@@ -29,9 +29,9 @@ function getCurrentItem(accessToken) {
     // Construct the REST URL to the current item
     // Details for formatting the URL can be found at 
     // https://msdn.microsoft.com/office/office365/APi/mail-rest-operations#get-a-message-rest
-    var getMessageUrl = Office.context.mailbox.restUrl +
-        '/v2.0/me/messages/' + itemId;
-
+    //var getMessageUrl = Office.context.mailbox.restUrl +
+    //    '/v2.0/me/messages/' + itemId;
+    var getMessageUrl = "https://graph.microsoft.com/beta/me/messages" + itemId;
     $.ajax({
         url: getMessageUrl,
         dataType: 'json',
@@ -57,102 +57,5 @@ function getItemRestId() {
         );
     }
 }
-function getSoapEnvelope(request) {
-    // Wrap an Exchange Web Services request in a SOAP envelope.
-    var result =
 
-        '<?xml version="1.0" encoding="utf-8"?>' +
-        '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"' +
-        '               xmlns:xsd="http://www.w3.org/2001/XMLSchema"' +
-        '               xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"' +
-        '               xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types">' +
-        '  <soap:Header>' +
-        '    <RequestServerVersion Version="Exchange2013" xmlns="http://schemas.microsoft.com/exchange/services/2006/types" soap:mustUnderstand="0" />' +
-        '  </soap:Header>' +
-        '  <soap:Body>' +
-
-        request +
-
-        '  </soap:Body>' +
-        '</soap:Envelope>';
-
-    return result;
-}
-
-function GetFolder() {
-    var results =
-
-        '      <GetFolder xmlns="http://schemas.microsoft.com/exchange/services/2006/messages">' +
-        '      <FolderShape>' +
-        '        <t:BaseShape>AllProperties</t:BaseShape>' +
-        '        <t:AdditionalProperties>' +
-        '          <t:ExtendedFieldURI PropertyTag="26180" PropertyType="String" />' +
-        '        </t:AdditionalProperties>' +
-        '      </FolderShape>' +
-        '      <FolderIds>' +
-        '        <t:DistinguishedFolderId Id="inbox" />' +
-        '      </FolderIds>' +
-        '    </GetFolder>';
-
-    return results;
-}
-
-function GetFolderCnt() {
-    var results =
-        ' <FindFolder Traversal="Deep" xmlns="http://schemas.microsoft.com/exchange/services/2006/messages">' +
-        '<FolderShape>' +
-        '<t:BaseShape>Default</t:BaseShape>' +
-        '</FolderShape>' +
-        '<IndexedPageFolderView MaxEntriesReturned="1" Offset="0" BasePoint="Beginning" />' +
-        '<ParentFolderIds>' +
-        ' <t:DistinguishedFolderId Id="msgfolderroot"/>' +
-        '</ParentFolderIds>' +
-        ' </FindFolder>';
-    return results;
-}
-function callbackGetFolder(asyncResult) {
-    var is_chrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
-    if (is_chrome) {
-        // var xml = $.parseXML(asyncResult.value),
-        //  $xml = $(xml);
-        //  totalCount = $xml.find('TotalCount');
-        //  $('#InboxItemCount').text("Inbox Total Item Count : " + totalCount[0].textContent);
-        var parser = new DOMParser();
-        var doc = parser.parseFromString(asyncResult.value, "text/xml");
-        var values = doc.getElementsByTagName("Value");
-        var totalCount = doc.getElementsByTagName("TotalCount");
-        var UnreadCount = doc.getElementsByTagName("UnreadCount");
-        $('#ServerName').text("Mailbox ServerName : " + values[0].textContent);
-        $('#InboxItemCount').text("Inbox Total Item Count : " + totalCount[0].textContent);
-        $('#InboxUnread').text("Inbox Unread Item Count : " + UnreadCount[0].textContent);
-    }
-    else {
-        var parser = new DOMParser();
-        var doc = parser.parseFromString(asyncResult.value, "text/xml");
-        var values = doc.getElementsByTagName("t:Value");
-        var totalCount = doc.getElementsByTagName("t:TotalCount");
-        var UnreadCount = doc.getElementsByTagName("t:UnreadCount");
-        $('#ServerName').text("Mailbox ServerName : " + values[0].textContent);
-        $('#InboxItemCount').text("Inbox Total Item Count : " + totalCount[0].textContent);
-        $('#InboxUnread').text("Inbox Unread Item Count : " + UnreadCount[0].textContent);
-    }
-
-
-}
-function callbackFindFolder(asyncResult) {
-    var is_chrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
-    if (is_chrome) {
-        var xml = $.parseXML(asyncResult.value),
-            $xml = $(xml),
-            $RootFolder = $xml.find('RootFolder');
-        $('#TotalFolderCount').text("Total Mailbox Folder Count : " + $RootFolder[0].attributes.getNamedItem("TotalItemsInView").textContent);
-    }
-    else {
-
-        var parser = new DOMParser();
-        var doc = parser.parseFromString(asyncResult.value, "text/xml");
-        var values = doc.childNodes[0].getElementsByTagName("m:RootFolder");
-        $('#TotalFolderCount').text("Total Mailbox Folder Count : " + values[0].attributes.getNamedItem("TotalItemsInView").textContent);
-    }
-}
 
