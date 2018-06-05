57f4df68e88d2216df17189359fba486ef9ae60e
Glen Scales <gscales@datarumble.com>
2018-06-05 13:03:51 +1000

    Made Ie compliant

--- SenderSentiment/MessageRead.js
+++ SenderSentiment/MessageRead.js
@@ -24,8 +24,8 @@
 
     function getSentiment(accessToken) {
         var rightNow = new Date();
-        rightNow.setDate(rightNow.getDate() - 30);    
-        var GETURL = "https://outlook.office.com/api/v2.0/me/MailFolders/Inbox/messages/?$select=ReceivedDateTime,Sender&$Top=1000&$filter=from/emailAddress/address eq '" + Office.context.mailbox.item.sender.emailAddress + "' And receivedDateTime ge " + rightNow.toISOString() + "&$expand=SingleValueExtendedProperties($filter=PropertyId eq 'String {00062008-0000-0000-C000-000000000046} Name EntityExtraction/Sentiment1.0')";       
+        rightNow.setDate(rightNow.getDate() - 30);
+        var GETURL = "https://outlook.office.com/api/v2.0/me/MailFolders/Inbox/messages/?$select=ReceivedDateTime,Sender&$Top=1000&$filter=from/emailAddress/address eq '" + Office.context.mailbox.item.sender.emailAddress + "' And receivedDateTime ge " + rightNow.toISOString() + "&$expand=SingleValueExtendedProperties($filter=PropertyId eq 'String {00062008-0000-0000-C000-000000000046} Name EntityExtraction/Sentiment1.0')";
         GETURL = encodeURI(GETURL);
         $.ajax({
             type: "GET",
@@ -41,9 +41,8 @@
     }
 
     function uuidv4() {
-        return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
-            (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-        )
+        var uniqueId = Math.random().toString(36).substring(2) + (new Date()).getTime().toString(36);
+        return uniqueId;
     }
 
     function DisplayResults(entry, accessToken) {
@@ -51,24 +50,24 @@
         var Positive = 0;
         var Last7Negative = 0;
         var Last7Positive = 0;
-        var Last7Count = 0;        
+        var Last7Count = 0;
         var last7Date = new Date();
         var BatchId = uuidv4();
-        var BatchPost = "";    
+        var BatchPost = "";
         BatchPost = BatchPost + "--batch_" + BatchId + "\n";
         var BatchCount = 0;
-        last7Date.setDate(last7Date.getDate() - 7); 
+        last7Date.setDate(last7Date.getDate() - 7);
         var index = 0;
         for (index = 0; index < entry.value.length; ++index) {
             var ParsedDate = new Date(entry.value[index].ReceivedDateTime);
             if (ParsedDate > last7Date) {
                 Last7Count++;
             }
-            if (entry.value[index].hasOwnProperty("SingleValueExtendedProperties")) {               
+            if (entry.value[index].hasOwnProperty("SingleValueExtendedProperties")) {
                 BatchPost = BatchPost + "Content-Type:application/http\n";
-                BatchPost = BatchPost + "Content-Transfer-Encoding:binary\n\n";  
+                BatchPost = BatchPost + "Content-Transfer-Encoding:binary\n\n";
                 var GETURL = "GET https://outlook.office.com/api/v2.0/me/MailFolders/Inbox/messages('" + entry.value[index].Id + "')/?$select=ReceivedDateTime,Sender&$expand=SingleValueExtendedProperties($filter=PropertyId eq 'String {00062008-0000-0000-C000-000000000046} Name EntityExtraction/Sentiment1.0')";
-               // GETURL = encodeURI(GETURL);
+                // GETURL = encodeURI(GETURL);
                 BatchPost = BatchPost + GETURL + " HTTP/1.1\n\n";
                 BatchPost = BatchPost + "--batch_" + BatchId + "\n";
                 BatchCount++;
@@ -118,12 +117,12 @@
 
         }
         if (BatchCount > 0) {
-            
+
             var headerc = "multipart/mixed; charset=utf-8; boundary=batch_" + BatchId;
             var PostURL = "https://outlook.office.com/api/v2.0/me/$batch";
             $.ajax({
                 type: "POST",
-                contentType: headerc ,
+                contentType: headerc,
                 url: PostURL,
                 async: false,
                 data: BatchPost,
@@ -151,52 +150,42 @@
                     }
                 }
             }).fail(function (error) {
-            //todo handle error
+                //todo handle error
             });
         }
 
-        var html = `
-              <div class="ms-Table-row">
-              <span class="ms-Table-cell"></span>
-              <span class="ms-Table-cell"></span>
-              </div>
-`;
-        var html = html + `
-                  <div class="ms-Table-row">
-                  <span class="ms-Table-cell ms-fontWeight-semibold">TotalCount Last 30 Days</span>
-                  <span id="last30" class="ms-Table-cell">`;
-            html = html + entry.value.length + "</span ></div >";
-            var html = html + `
-                  <div class="ms-Table-row">
-                  <span class="ms-Table-cell ms-fontWeight-semibold">TotalCount Last 7 Days</span>
-                  <span id="last7" class="ms-Table-cell">`;
-            html = html + Last7Count + "</span ></div >";
-            $('#sentTable').append(html);
-            var html = "";
-            var html = html + `
-              <div class="ms-Table-row">
-              <span class="ms-Table-cell"></span>
-              <span class="ms-Table-cell"  style="font-size: 14px;">Positive &#x1f603;</span>
-              <span class="ms-Table-cell"  style="font-size: 14px;">Negative &#x1f641;</span>
-              </div>
-`;
-            var html = html + `
-                  <div class="ms-Table-row">
-                  <span class="ms-Table-cell ms-fontWeight-semibold">Last 7 Days</span>
-                  <span id="pCount7" class="ms-Table-cell" style="text-align:center;">`;
-            html = html + Last7Positive + "</span >";  
-            html = html + "<span id=\"nCount7\" class=\"ms-Table-cell\" style=\"text-align:center;\">" + Last7Negative + "</span ></div >";  
-            var html = html + `
-                  <div class="ms-Table-row">
-                  <span class="ms-Table-cell ms-fontWeight-semibold">Last 30 Days</span>
-                  <span id="pCount" class="ms-Table-cell" style="text-align:center;">`;
-            html = html + Positive + "</span >";
-            html = html + "<span id=\"nCount\" class=\"ms-Table-cell\" style=\"text-align:center;\">" + Negative + "</span ></div >";
-            $('#sentimentTable').append(html);
-    }
+        var html = "<div class=\"ms-Table-row\">";
+        html = html + "<span class=\"ms-Table-cell\"></span>";
+        html = html + "<span class=\"ms-Table-cell\"></span>";
+        html = html + "</div>";
 
-    function sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
+        html = html + "<div class=\"ms-Table-row\">";
+        html = html + "<span class=\"ms-Table-cell ms-fontWeight-semibold\">TotalCount Last 30 Days</span>";
+        html = html + "<span id=\"last30\" class=\"ms-Table-cell\">";
+        html = html + entry.value.length + "</span ></div >";
+        html = html + "<div class=\"ms-Table-row\">";
+        html = html + "<span class=\"ms-Table-cell ms-fontWeight-semibold\">TotalCount Last 7 Days</span>";
+        html = html + "<span id=\"last7\" class=\"ms-Table-cell\">";
+        html = html + Last7Count + "</span ></div >";
+        $('#sentTable').append(html);
+        var html = "";
+        html = html + "<div class=\"ms-Table-row\">";
+        html = html + "<span class=\"ms-Table-cell\"></span>";
+        html = html + "<span class=\"ms-Table-cell\"  style=\"font-size: 14px;\">Positive &#x1f603;</span>";
+        html = html + "<span class=\"ms-Table-cell\"  style=\"font-size: 14px;\">Negative &#x1f641;</span>";
+        html = html + "</div>";
+
+        html = html + "<div class=\"ms-Table-row\">";
+        html = html + "<span class=\"ms-Table-cell ms-fontWeight-semibold\">Last 7 Days</span>";
+        html = html + "<span id=\"pCount7\" class=\"ms-Table-cell\" style=\"text-align:center;\">";
+        html = html + Last7Positive + "</span >";
+        html = html + "<span id=\"nCount7\" class=\"ms-Table-cell\" style=\"text-align:center;\">" + Last7Negative + "</span ></div >";
+        html = html + "<div class=\"ms-Table-row\">";
+        html = html + "<span class=\"ms-Table-cell ms-fontWeight-semibold\">Last 30 Days</span>";
+        html = html + "<span id=\"pCount\" class=\"ms-Table-cell\" style=\"text-align:center;\">";
+        html = html + Positive + "</span >";
+        html = html + "<span id=\"nCount\" class=\"ms-Table-cell\" style=\"text-align:center;\">" + Negative + "</span ></div >";
+        $('#sentimentTable').append(html);
     }
     // Helper function for displaying notifications
     function showNotification(header, content) {
