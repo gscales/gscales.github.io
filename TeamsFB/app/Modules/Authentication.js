ebc0b6a0d95479f7b877b77693d3260d9e16c5c9
Glen Scales <gscales@datarumble.com>
2018-09-12 10:30:49 +1000

    TeamsFB

---
+++ TeamsFB/app/Modules/Authentication.js
@@ -0,0 +1,28 @@
+
+(function () {
+
+    const GetTokenFromSkype = (TeamsClient) => {
+        return new Promise(
+            (resolve, reject) => {
+
+            }
+        );
+    }
+
+
+    async function authtest() {
+        try {
+            return "test12345";
+            
+        }
+        catch (error) {
+            //handle Error **To Do **
+        }
+    }
+
+    module.exports.authtest = function () {
+        return authtest();
+    }
+
+}());
+
