08347a5a7e2263b2804f36570af125fcefa0ccd4
Glen Scales <gscales@datarumble.com>
2018-10-10 12:07:45 +1100

    Teams pf app

---
+++ TeamsPublicFolder/app/Config/appconfig.js
@@ -0,0 +1,11 @@
+const getConfig = () => {
+   var config = {
+        clientId : "2417973f-0680-4ea0-a844-e6d414cee4d4",
+        redirectUri : "/TeamsPublicFolder/app/silent-end.html",
+        authwindow :  "/TeamsPublicFolder/app/auth.html",
+	hostRoot: "https://gscales.github.io",
+	folderpath: "\\Childfolder\\childfolder",
+	ewsproxy: "https://459e25ee.ngrok.io/FolderItems",
+   };
+   return config;
+}
