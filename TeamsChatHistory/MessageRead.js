d898ea88589c348f6fad46ec751d0982369de71d
Glen Scales <gscales@datarumble.com>
2020-11-24 14:02:19 +1100

    fix7

--- TeamsChatHistory/MessageRead.js
+++ TeamsChatHistory/MessageRead.js
@@ -180,6 +180,12 @@
 
     function FindFolderRequest(){
         var RequestString =
+        '<?xml version="1.0" encoding="utf-8"?>' +
+        '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:m="http://schemas.microsoft.com/exchange/services/2006/messages" xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">' +
+        '  <soap:Header>' +
+        '    <t:RequestServerVersion Version="Exchange2016" />' +
+        '  </soap:Header>' +
+        '  <soap:Body>' +
         '<m:FindFolder Traversal="Shallow">' +
         '<m:FolderShape>' +
         '  <t:BaseShape>AllProperties</t:BaseShape>' +
@@ -197,6 +203,8 @@
         '  <t:DistinguishedFolderId Id="root" />' +
         '</m:ParentFolderIds>' +
         '</m:FindFolder>'
+        '  </soap:Body>' +
+        '</soap:Envelope>'
         return RequestString;
   
     }
